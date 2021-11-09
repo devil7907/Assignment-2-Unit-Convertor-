d7a7ea82ecacb92b50b67484e3e6469b0851a036
devil7907 <92261949+devil7907@users.noreply.github.com>
2021-11-09 23:01:11 +0500

    Link to Git Repositry

--- Assignment3/app/src/main/java/com/example/assignment_3/MainActivity.java
+++ Assignment3/app/src/main/java/com/example/assignment_3/MainActivity.java
@@ -2,19 +2,60 @@ package com.example.assignment_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1,btn2,btn3;
+    Button btn,btn1,btn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn2.findViewById(R.id.button2);
-        btn2.setOnClickListener();
+        btn1 = findViewById(R.id.button2);
+        btn=findViewById(R.id.button);
+        btn2=findViewById(R.id.button3);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoquiz();
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotopractice();
+            }
+        });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotorgepositry("https://www.google.com");
 
+            }
+        });
+
+
+    }
+
+    private void gotorgepositry(String s) {
+        Uri Webpage=Uri.parse(s);
+        Intent intent=new Intent(Intent.ACTION_VIEW,Webpage);
+        startActivity(intent);
+    }
+
+    private void gotopractice() {
+        Intent intent1=new Intent(this,practice.class);
+        startActivity(intent1);
+    }
+
+    private void gotoquiz() {
+        Intent intent=new Intent(this,quiz.class);
+        startActivity(intent);
     }
 }
