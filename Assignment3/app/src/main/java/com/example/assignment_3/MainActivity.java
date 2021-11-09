f86e7e887e4cfc984f567628bea0fda6779d5967
devil7907 <92261949+devil7907@users.noreply.github.com>
2021-11-09 21:09:09 +0500

    first commit

---
+++ Assignment3/app/src/main/java/com/example/assignment_3/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.assignment_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1,btn2,btn3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn2.findViewById(R.id.button2);
+        btn2.setOnClickListener();
+
+    }
+}
