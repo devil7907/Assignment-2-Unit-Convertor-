5b6533071b32a7e34ed68ee601f77f89d511d589
devil7907 <92261949+devil7907@users.noreply.github.com>
2021-11-13 15:44:41 +0500

    Practice Question-1 Added

--- Assignment3/app/src/main/java/com/example/assignment_3/practice.java
+++ Assignment3/app/src/main/java/com/example/assignment_3/practice.java
@@ -8,19 +8,39 @@ import android.view.View;
 import android.widget.Button;
 
 public class practice extends AppCompatActivity {
-    Button backbtn;
+    Button practiceq1,practiceq2,practiceq3,practiceq4,practiceq5,practiceq6,practiceq7,practiceq8,backbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
 
+        practiceq1=findViewById(R.id.button9);
+        practiceq2=findViewById(R.id.button10);
+        practiceq3=findViewById(R.id.button11);
+        practiceq4=findViewById(R.id.button12);
+        practiceq5=findViewById(R.id.button13);
+        practiceq6=findViewById(R.id.button14);
+        practiceq7=findViewById(R.id.button15);
+        practiceq8=findViewById(R.id.button16);
         backbtn=findViewById(R.id.button17);
+
+        practiceq1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoq1();
+            }
+        });
         backbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goback();
             }
         });
+    }
+
+    private void gotoq1() {
+        Intent intent=new Intent(this, practiceq1.class);
+        startActivity(intent);
     }
 
     private void goback() {
---
+++ Assignment3/app/src/main/java/com/example/assignment_3/practiceq1.java
@@ -0,0 +1,30 @@
+package com.example.assignment_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class practiceq1 extends AppCompatActivity {
+    Button backbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practiceq1);
+
+        backbtn=findViewById(R.id.button8);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goback();
+            }
+        });
+    }
+
+    private void goback() {
+    Intent intent=new Intent(this,practice.class);
+    startActivity(intent);
+    }
+}
