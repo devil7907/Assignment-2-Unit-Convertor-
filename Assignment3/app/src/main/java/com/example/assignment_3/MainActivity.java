4296a7cbb01f2a53834bddd23ffa566d96cc6468
devil7907 <92261949+devil7907@users.noreply.github.com>
2021-12-07 22:11:29 +0500

    Tool Bar & Menu Added

--- Assignment3/app/src/main/java/com/example/assignment_3/MainActivity.java
+++ Assignment3/app/src/main/java/com/example/assignment_3/MainActivity.java
@@ -1,25 +1,36 @@
 package com.example.assignment_3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn,btn1,btn2;
+    Button btn, btn1, btn2;
+    Toolbar mytoolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mytoolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar(mytoolbar);
+
         btn1 = findViewById(R.id.button3);
-        btn=findViewById(R.id.button);
-        btn2=findViewById(R.id.button2);
+        btn = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,24 +49,37 @@ public class MainActivity extends AppCompatActivity {
                 gotorgepositry("https://github.com/devil7907/Assignment-2-Unit-Convertor-");
 
             }
+
+
         });
 
 
     }
+    @Override
 
-    private void gotorgepositry(String s) {
-        Uri Webpage=Uri.parse(s);
-        Intent intent=new Intent(Intent.ACTION_VIEW,Webpage);
-        startActivity(intent);
+    public boolean onCreateOptionsMenu(Menu menu) {   // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 
-    private void gotopractice() {
-        Intent intent1=new Intent(this,practice.class);
-        startActivity(intent1);
-    }
 
-    private void gotoquiz() {
-        Intent intent=new Intent(this,quiz.class);
-        startActivity(intent);
-    }
-}
+
+
+
+
+            private void gotorgepositry (String s){
+            Uri Webpage = Uri.parse(s);
+            Intent intent = new Intent(Intent.ACTION_VIEW, Webpage);
+            startActivity(intent);
+        }
+
+        private void gotopractice () {
+            Intent intent1 = new Intent(this, practice.class);
+            startActivity(intent1);
+        }
+
+        private void gotoquiz () {
+            Intent intent = new Intent(this, quiz.class);
+            startActivity(intent);
+        }
+        }
